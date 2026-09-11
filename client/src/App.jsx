3d424e7a524913f858289dfb8339aefbdc6f11a2
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-09-11 12:22:32 +0700

    feat: add query on products route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({
+    name: "",
+    price: 0,
+    quantity: 1,
+  });
 
   return (
-    <div>
-      <form>
-        <label htmlFor=""></label>
+    <div className="bg-blue-400 min-h-screen text-center">
+      <h1 className="font-bold uppercase text-white">Products</h1>
+      <form className="w-fit flex flex-col gap-2 mx-auto p-10 rounded-3xl bg-blue-200 text-start font-semibold">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="price">Price</label>
+        <input type="text" id="price" />
+        <label htmlFor="quantity">Quantity</label>
+        <input type="text" id="quantity" />
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
